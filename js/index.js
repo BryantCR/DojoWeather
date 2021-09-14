ea002c73fe9e06c18db558f3fa4e4e8ee521e17e
BryantCR <bryancvargas20@gmail.com>
2021-09-14 12:22:30 -0600

    All working good

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-function cityalert (){
-    alert("alert");
+function cityalert (event){
+    alert("Loading Weather Report");
 }
 let citybutton = document.querySelectorAll('.city');
 for( let i = 0; i < citybutton.length; i++){
     citybutton[i].addEventListener("click", cityalert);
 }
 
-function removeCookie(element){
+function removeCookie(){
+    let cookie = document.querySelector('.block3')
+    cookie.remove();
+}
+
+function degrees(){
     
 }
 
