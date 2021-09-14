3b546bf46df6f8a09aa7a3352f0c03ce93c9cb0d
BryantCR <bryancvargas20@gmail.com>
2021-09-14 13:58:27 -0600

    Functions process

--- js/index.js
+++ js/index.js
@@ -11,8 +11,30 @@ function removeCookie(){
     cookie.remove();
 }
 
-function degrees(){
-    
+function degrees(element){
+    let selectdegrees = element.value/*document.querySelectorAll('.selector').value;*/
+    console.log(selectdegrees)
+    if(selectdegrees === "Fahrenheit"){
+        console.log("AHHHHH")
+        numberDegrees.innerHTML = "75°"
+        numberDegrees2.innerHTML = "65°"
+    }
+    else{
+        if(selectdegrees === "Celsius"){
+            console.log("OHHHHH")
+            numberDegrees.innerHTML = "24°"
+            numberDegrees2.innerHTML = "18°"
+        }
+    }
+}
+
+let numberDegrees = document.querySelector('.gradered');
+for( let i = 0; i < numberDegrees.length; i++){
+    numberDegrees[i].addEventListener("onchange", degrees);
+}
+let numberDegrees2 = document.querySelector('.gradeblue');
+for( let i = 0; i < numberDegrees2.length; i++){
+    numberDegrees2[i].addEventListener("onchange", degrees);
 }
 
 /*let button = document.querySelector('.acceptbutton');
